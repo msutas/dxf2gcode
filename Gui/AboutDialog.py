c25c85766be60a74712a7c8e5b70d26e3ed39621
Christian Kohlöffel <christian-kohloeffel@t-online.de>
2013-09-03 10:24:38 +0000

    Added changes for Cutter Compensation based on current trunk to this branch.

---
+++ Gui/AboutDialog.py
@@ -0,0 +1,40 @@
+from PyQt4 import QtGui, QtCore
+import logging
+logger = logging.getLogger("Gui.AboutDialog") 
+
+class myAboutDialog(QtGui.QDialog):
+    
+    def __init__(self, title="Test", message="Test Text"):
+        super(myAboutDialog, self).__init__()
+
+        self.title = title
+        self.message = message
+        
+        self.initUI()
+        
+    def initUI(self):      
+
+        vbox = QtGui.QVBoxLayout(self)
+        grid1 = QtGui.QGridLayout()
+        grid1.setSpacing(10)
+
+        self.text = QtGui.QTextBrowser()
+        self.text.setReadOnly(True)
+        self.text.setOpenExternalLinks(True)
+        self.text.append(self.message)
+        self.text.moveCursor(QtGui.QTextCursor.Start)
+        self.text.ensureCursorVisible()
+        
+        vbox.addWidget(self.text)
+       
+        self.setLayout(vbox)
+        self.setMinimumSize(550, 450)
+        self.resize(550, 600)
+        self.setWindowTitle(self.title)
+        iconWT = QtGui.QIcon()
+        iconWT.addPixmap(QtGui.QPixmap(":images/DXF2GCODE-001.ico"),
+                         QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(QtGui.QIcon(iconWT))
+        
+        self.exec_()
+    
